40250b4cd09cf7e09e746148f418f9d9b8a2d789
Zhuorui Yang <zhuoruiyang@gmail.com>
2017-10-18 13:02:24 -0400

    initial commit

---
+++ src/ClientOnClass.java
@@ -0,0 +1,72 @@
+import java.net.*;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.io.*;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class ClientOnClass {
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		try{
+			int port_client = 9995;
+			Socket msocket = new Socket(InetAddress.getLocalHost(), port_client);
+			
+			ObjectInputStream ois = new ObjectInputStream(msocket.getInputStream());
+			PublicKey serverPublicKey = (PublicKey) ois.readObject();
+			
+			DataOutputStream dos = new DataOutputStream(msocket.getOutputStream());
+			
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+			keyGen.initialize(512);
+			KeyPair clientKey = keyGen.generateKeyPair();
+			
+			ObjectOutputStream oos = new ObjectOutputStream(msocket.getOutputStream());
+	
+			System.out.println(clientKey.getPublic());
+			oos.writeObject(clientKey.getPublic());
+			oos.flush();
+			
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, serverPublicKey);
+			String plainText = "I have a secret";
+			System.out.println(new String(plainText.getBytes()));
+			
+			byte[] cipherText = cipher.doFinal(plainText.getBytes());
+			dos.writeInt(cipherText.length);
+			dos.write(cipherText);
+			dos.flush();
+			
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			System.out.println("The message digest algorithm used is " + md.getAlgorithm());
+			System.out.println("The provider which actually implements this algorithm is " + md.getProvider());
+			md.update(plainText.getBytes());
+			byte[] messageDigestMD5 = md.digest();
+			dos.writeInt(messageDigestMD5.length);
+			dos.write(messageDigestMD5);
+			dos.flush();
+			
+			DataInputStream dis = new DataInputStream(msocket.getInputStream());
+			String result = dis.readUTF();
+			System.out.println("The result returned by server is " + result);
+			String result2 = dis.readUTF();
+			System.out.println("The integrity of message is checked with " + result2);
+	
+			dis.close();
+			msocket.close();
+		}catch(IOException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e)
+		{
+			e.getStackTrace();
+		}
+	}
+	
+	
+}
---
+++ src/ServerOnClass.java
@@ -0,0 +1,83 @@
+import java.net.*;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.io.*;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class ServerOnClass {
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException, ClassNotFoundException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+	{
+		int port_server = 9995;
+		ServerSocket mss = new ServerSocket(port_server);
+		
+		try {
+			while(true) {
+				Socket ms = mss.accept();
+				
+				try {
+					KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+					keyGen.initialize(512);
+					KeyPair serverKey = keyGen.generateKeyPair();
+					// send server's public key to client
+					ObjectOutputStream oos = new ObjectOutputStream(ms.getOutputStream());
+					oos.writeObject(serverKey.getPublic());
+					oos.flush();
+					// get client's public key
+					ObjectInputStream ois = new ObjectInputStream(ms.getInputStream());
+					PublicKey clientPublicKey = (PublicKey) ois.readObject();
+					System.out.println(clientPublicKey);
+			
+					DataInputStream dis = new DataInputStream(ms.getInputStream());
+					int length = dis.readInt();
+					
+					byte[] cipherText = null;
+					if(length>0) {
+						cipherText = new byte[length];
+					    dis.readFully(cipherText, 0, cipherText.length); // read the message
+					}
+					
+					Cipher cipher = Cipher.getInstance("RSA");
+					cipher.init(Cipher.DECRYPT_MODE, serverKey.getPrivate());
+					String plainText = new String(cipher.doFinal(cipherText));
+					String result = "Echo from server is: " + plainText;
+					
+					MessageDigest md = MessageDigest.getInstance("MD5");
+					int lengthMd = dis.readInt();
+					
+					byte[] receivedMd = null;
+					if(lengthMd>0) {
+						receivedMd = new byte[lengthMd];
+					    dis.readFully(receivedMd, 0, receivedMd.length); // read the message
+					}
+					md.update(plainText.getBytes());
+					byte[] messageDigestMD5 = md.digest();
+			
+					DataOutputStream dos = new DataOutputStream(ms.getOutputStream());
+					dos.writeUTF(result);
+					if(Arrays.equals(messageDigestMD5, receivedMd)) {
+						dos.writeUTF("yes");
+					}else{
+						dos.writeUTF("no");
+					}
+					
+					dos.flush();
+					dos.close();
+					oos.close();
+				} finally {
+					ms.close();
+				}
+			}
+		} finally {
+			mss.close();
+		}
+	}	
+}
